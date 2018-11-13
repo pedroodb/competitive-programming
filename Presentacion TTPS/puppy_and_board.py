ca9ba7d27b90c2a7d99b379e96665d989eab5458
Pedro <dalbianco.pedro@gmail.com>
2018-11-12 23:48:50 -0300

    Presentacion v2

--- Presentacion TTPS/puppy_and_board.py
+++ Presentacion TTPS/puppy_and_board.py
@@ -1,17 +1,18 @@
 from functools import reduce
 
 def movimientos(i,j):
-    movs = [(movsI,j) for movsI in range(0,i)]
-    movs.extend([(i,movsJ) for movsJ in range(0,j)])
-    return movs
+    for movsI in range(0,i):
+        yield (movsI,j)
+    for movsJ in range(0,j):
+        yield (i,movsJ)
 
 def posicionGanadora(i,j,memo):
     if memo[i][j] is None:
-        memo[i][j] = not reduce(lambda res, sigMov: res and posicionGanadora(sigMov[0],sigMov[1],memo), movimientos(i,j), True)
+        memo[i][j] = reduce(lambda res, sigMov: res or not posicionGanadora(sigMov[0],sigMov[1],memo), movimientos(i,j), False)
     return memo[i][j]
 
 testCases = int(input())
-memo = [[False if i == 0 and j == 0 else None for i in range(3)] for j in range(4)]
+memo = [[None for i in range(3)] for j in range(4)]
 for tc in range(testCases):
     n,m = list(map(int,input().split(' ')))
     print('Tuzik' if posicionGanadora((n-1) % 4, (m-1) % 3, memo) else 'Vanya')
