359881c85da9af3aa8e41a1e1d20aff3b4729722
fedediclaudio <37820912+fedediclaudio@users.noreply.github.com>
2018-11-21 11:20:56 -0300

    Edit Distance + Interseccion de segmentos

--- algorithms/dynamic_programming/edit_distance.py
+++ algorithms/dynamic_programming/edit_distance.py
@@ -12,9 +12,33 @@ def editDistance(str1,str2,delCost,insCost,editCost):
                 memo[i][j] = min(memo[i-1][j-1]+(0 if str1[i-1] == str2[j-1] else editCost),memo[i-1][j]+delCost,memo[i][j-1]+insCost)
     return memo[-1][-1]
 
+#Bottom-up iterative implementation including path
+
+def editDistanceWithPath(str1,str2,delCost,insCost,editCost):
+    memo = [[[0, []] for _ in range(len(str2)+1)] for _ in range(len(str1)+1)]
+    for i in range(0,len(str1)+1):
+        for j in range(0,len(str2)+1):
+            if j == 0:
+                memo[i][j] = [delCost*i, memo[i][j][1] + [("d", str1[i-1])]]
+            elif i == 0:
+                memo[i][j] = [insCost*j, memo[i][j][1] + [("a", str2[j-1])]]
+            else:
+                mincost = memo[i-1][j-1]
+                if not str1[i-1] == str2[j-1]:
+                    mincost[0] += editCost
+                    mincost[1] += [("m", str1[i-1], str2[j-1])]
+                if memo[i-1][j][0]+delCost < mincost[0]:
+                    mincost[0] = memo[i-1][j][0]+delCost
+                    mincost[1] += [("d", str1[i-1])]
+                if memo[i][j-1][0]+insCost < mincost[0]:
+                    mincost[0] = memo[i][j-1][0]+insCost
+                    mincost[1] += [("a", str2[j-1])]
+    return memo[-1][-1]
+
+
 #No entra en spoj, preguntar
 #Preguntar como se guardaria el recorrido
 
 tc = int(input())
 for t in range(tc):
-    print(editDistance(input(),input(),1,1,1))
+    print(editDistanceWithPath(input(),input(),1,1,1))
