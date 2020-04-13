be31e0d2caded49b7f2fc08900eadf8ab2a39a82
pedroodb <dalbianco.pedro@gmail.com>
2020-04-13 16:08:59 -0300

    Correcciones pothole

--- solutions/max_flow/pothole.py
+++ solutions/max_flow/pothole.py
@@ -16,29 +16,42 @@ the residual capacity.
 
 import queue
 
-graph = [[-1 for i in range(201)] for j in range(201)]
+V = 201
+capacities = [[-1 for i in range(V)] for j in range(V)]
 
-def maxFlow(v):
-    result = 0
-    while True:
-        pathCapacity = findPath(v)
-        if pathCapacity != 0:
-            result += pathCapacity
-        else:
-            return result
 
-def findPath(vSize):
-    visited = [False for i in range(vSize)]
-    prev = [-1 for i in range(vSize)]
+def maxFlow(s,t):
+
+    flow = 0
+    prev = [-1 for i in range(V)]
+    pathCapacity = findPath(s,t,prev)
+
+    while pathCapacity != 0:
+
+        flow += pathCapacity
+        #Update residual capacities
+        where = t
+        while prev[where] != -1:
+            capacities[prev[where]][where] -= pathCapacity
+            capacities[where][prev[where]] += pathCapacity
+            where = prev[where]
+
+        prev = [-1 for i in range(V)]
+        pathCapacity = findPath(s,t,prev)
+
+    return flow
+
+def findPath(s,t,prev):
+    visited = [False for i in range(V)]
     q = queue.Queue()
-    q.put(0)
+    q.put(s)
     while not q.empty():
         v = q.get()
         # Sink is the last vertex
-        if v == vSize-1:
+        if v == t:
             break
         visited[v] = True
-        adjacents = list(map(lambda x: x[0],filter(lambda x: x[1] > 0 and not visited[x[0]],enumerate(graph[v][:vSize]))))
+        adjacents = list(map(lambda x: x[0],filter(lambda x: x[1] > 0 and not visited[x[0]],enumerate(capacities[v][:V]))))
         for adj in adjacents:
             visited[adj] = True
             prev[adj] = v
@@ -46,40 +59,34 @@ def findPath(vSize):
 
     #Get pathCapacity as the lowest capacity from edges in the path
     pathCapacity = 0
-    where = vSize-1
+    where = t
     while prev[where] != -1:
-        pathCapacity = min([graph[prev[where]][where],pathCapacity]) if pathCapacity != 0 else graph[prev[where]][where]
+        pathCapacity = min([capacities[prev[where]][where],pathCapacity]) if pathCapacity != 0 else capacities[prev[where]][where]
         where = prev[where]
     
-    #Update residual capacities
-    where = vSize-1
-    while prev[where] != -1:
-        graph[prev[where]][where] -= pathCapacity
-        graph[where][prev[where]] += pathCapacity
-        where = prev[where]
     return pathCapacity
 
 def resetGraph():
     for i in range(200):
         for j in range(200):
-            graph[i][j] = -1
+            capacities[i][j] = -1
 
 def buildGraph(v):
     resetGraph()
     for i in range(0,v-1):
-        ady = list(map(lambda x: int(x)-1,input().split(' ')[1:]))
+        adj = list(map(lambda x: int(x)-1,input().split(' ')[1:]))
         if i == 0:
-            maxval = len(ady)
-        for j in ady:
-            graph[i][j] = (1 if (i == 0 or j == v-1) else maxval)
-            graph[j][i] = 0
+            maxval = len(adj)
+        for j in adj:
+            capacities[i][j] = (1 if (i == 0 or j == v-1) else maxval)
+            capacities[j][i] = 0
 
 tc = int(input())
 for i in range(tc):
-    v = int(input())
-    if v != 0:
-        buildGraph(v)
-        print(maxFlow(v))
+    V = int(input())
+    if V != 0:
+        buildGraph(V)
+        print(maxFlow(0,V-1))
     else:
         print(0)
     input()
